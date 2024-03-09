a04ed89ff5aaa6d59e8a7a3a484d208ea25ee7f5
Johan Fernando Aponte Valencia <wdjohanwd@gmail.com>
2024-03-09 23:55:21 +0100

    Basura eliminada y encapsulacion de preguntas.py

--- Preguntas.py
+++ Preguntas.py
@@ -2,10 +2,10 @@ import json
 from random import sample
 
 class Preguntas:
-    preguntas: dict
-    categorias: list
-    categoria_actual: str
-    pregunta: dict
+    __preguntas: dict
+    __categorias: list
+    __categoria_actual: str
+    __pregunta: dict
 
     def __init__(self) -> None:
         #Abre el archivo donde se encuentra las preguntas y las guarda.
@@ -13,22 +13,22 @@ class Preguntas:
         
         with open('.\\cosas No CLASES\\preguntas.json', 'r', encoding='utf-8') as archivo:
 
-            self.preguntas = json.load(archivo)
+            self.__preguntas = json.load(archivo)
 
-        self.categorias = list(self.preguntas.keys())
-        self.categoria_actual = ''
-        self.pregunta = {}
+        self.__categorias = list(self.__preguntas.keys())
+        self.__categoria_actual = ''
+        self.__pregunta = {}
     
     def elegir_pregunta(self, num: int) -> None:
         #Elige una pregunta aleatoria.
         
         
-        self.categoria_actual = self.categorias[num-1]
-        self.pregunta = sample(self.preguntas[self.categoria_actual], 1)[0]
+        self.__categoria_actual = self.__categorias[num-1]
+        self.__pregunta = sample(self.__preguntas[self.__categoria_actual], 1)[0]
 
     def obtener_pregunta_actual(self) -> tuple:
         #Devuelve la pregunta actual y su categoría.
         
         
-        return self.pregunta, self.categoria_actual
+        return self.__pregunta, self.__categoria_actual
  
