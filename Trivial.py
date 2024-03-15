6158cb00025b41bf71f5023a139de5e853dfbefb
Johan Fernando Aponte Valencia <wdjohanwd@gmail.com>
2024-03-15 23:09:32 +0100

    Casi finalizado, falta type hints, comentarios, excepciones y alguna cosa más

--- Trivial.py
+++ Trivial.py
@@ -2,25 +2,39 @@ from Jugador import Jugador
 from Tablero import Tablero
 from Historial import Historial
 from Vista import Vista
+from random import sample
 
 if __name__ == "__main__":
-    while True:
+    jugar_de_nuevo=True
+    mantener_jugadores=False
+    while jugar_de_nuevo:
         Vista.imprimir_inicio()
-
+        eleccion_inicio=Vista.preguntar_eleccion_inicio()
+        if eleccion_inicio == 1:
+            pass
+        elif eleccion_inicio ==2:
+            Vista.imprimir_inicio2()
+        if mantener_jugadores ==False:
+            jugadores=[]
+            while True:
+                try:
+                    num_jugadores=Vista.preguntar_jugador()
+                    if num_jugadores>= 2 and num_jugadores <=6:
+                        for i in range(num_jugadores):
+                            nombre = Vista.bucle_jugador(i)
+                            jugadores.append(Jugador(nombre))
+                        break
+                    else:
+                        Vista.mostrar_cantidad_jugadores_posibles()
+                except ValueError:
+                    Vista.errores_excepciones_value()
+                except:
+                    Vista.errores_excepciones_general
+        
+        jugadores=sample(jugadores,num_jugadores)
+        Vista.mostrar_orden_jugadores(jugadores)
         
         
-        jugadores=[]
-        while True:
-            num_jugadores=Vista.preguntar_jugador()
-            if num_jugadores>= 2 and num_jugadores <=6:
-                for i in range(num_jugadores):
-                    nombre = Vista.bucle_jugador(i)
-                    jugadores.append(Jugador(nombre))
-                break
-            else:
-                Vista.mostrar_cantidad_jugadores_posibles()
-        
-          
         tableros = [Tablero(jugador) for jugador in jugadores]
         
         # Iteramos hasta que uno de los jugadores gane
@@ -54,7 +68,18 @@ if __name__ == "__main__":
                 resultados=Vista.guardar_resultado(Jugador,tablero,jugadores,tableros)
                 Historial.guardar_en_historial('\n'.join(resultados))
                 break
-        break
+            
+        final, mantener_jugadores=Vista.pregunta_final()
+        
+        if final.lower() =='si':
+            jugar_de_nuevo=True
+        else:
+            jugar_de_nuevo=False
+        
+        if mantener_jugadores.lower() == 'si':
+            mantener_jugadores=True
+        else:
+            mantener_jugadores=False
     
     
     
--- Vista.py
+++ Vista.py
@@ -3,11 +3,9 @@ from termcolor import colored
 
 class Vista:
     
-    
     #Métodos para el main
     
-    
-    def imprimir_inicio():
+    def imprimir_inicio():#1
 
         print(colored('''
               BIENVENIDOS AL TRIVIAL:
@@ -22,9 +20,24 @@ class Vista:
         poner a prueba tus conocimientos en seis categorías diferentes: deporte, 
         geografía, arte, literatura, ciencia y entretenimiento. El objetivo es 
         ser el primer jugador en recolectar los seis quesos, uno de cada categoría,
-        dispersos a lo largo del tablero de 42 casillas.
+        dispersos a lo largo del tablero de 42 casillas.'''
 
-        Instrucciones del Juego:
+        print(texto)
+        
+        time.sleep(2)
+    
+    def preguntar_eleccion_inicio():#2
+        eleccion = int(input('''
+                         █████████████████████████████████████
+                         █          ¿Qué deseas hacer?       █
+                         █ 1. Comenzar a jugar directamente. █
+                         █ 2. Leer primero las instrucciones.█
+                         █████████████████████████████████████
+                         '''))
+        return eleccion
+    
+    def imprimir_inicio2():#3
+        texto2='''Instrucciones del Juego:
 
         1. ''' + colored('Preparación:', 'blue') + ''' Reúne a tus amigos o familiares para formar un grupo
         de jugadores. Cada jugador seleccionará un peón y colocará su ficha
@@ -38,7 +51,7 @@ class Vista:
         3. ''' + colored('Categorías y Quesos:', 'yellow') + ''' A lo largo del tablero, hay seis casillas 
         especiales marcadas con los quesos de cada categoría. Por ejemplo, 
         en la casilla 7 encontrarás el queso de deporte, en la casilla 14 el 
-        de geografía, y así sucesivamente. Para ganar un queso, un jugador debe
+        de geografía, y así sucesivamente (de 7 en 7 casillas). Para ganar un queso, un jugador debe
         llegar a la casilla correspondiente y responder correctamente una 
         pregunta de esa categoría.
 
@@ -69,14 +82,10 @@ class Vista:
         ¡Prepárate para un emocionante desafío de cultura general y disfruta
         compitiendo por ser el maestro del queso en este juego único!
         '''
-        print(texto)
-        
+        print(texto2)
         time.sleep(2)
-                
-            
-            
     
-    def preguntar_jugador():
+    def preguntar_jugador():#4
         
         try:
             num_jugadores = int(input('Introduce el número de jugadores: '))
@@ -85,10 +94,7 @@ class Vista:
             
         return num_jugadores
 
-    def mostrar_cantidad_jugadores_posibles():
-        print('La cantidad mínima de jugadores es de 2 y la cantidad máxima de jugadores es 6')
-        
-    def bucle_jugador(i):
+    def bucle_jugador(i):#5
         nombre_vacio=True
         while nombre_vacio:
             nombre = input(f'Introduce el nombre del jugador {i+1}: ')
@@ -101,28 +107,63 @@ class Vista:
                 time.sleep(1)
                 
         return nombre
-       
-    def mostrar_turno(jugadores,i):
-        print(f"\nTurno de {jugadores[i].nombre}:")
+    
+    def mostrar_cantidad_jugadores_posibles():#6
+        print('La cantidad mínima de jugadores es de 2 y la cantidad máxima de jugadores es 6')
+        
+    def mostrar_orden_jugadores(jugadores):#7
+        time.sleep(1)
+        print('El orden de los jugadores se decidirá de manera aleatoria. Estos son los resultados...')
+        time.sleep(2)
+        lista_ordinal=['Primer','Segundo','Tercer','Cuarto','Quinto','Sexto']
+        cadena=''
+        for i in range(len(jugadores)):
+            cadena+=f'El {lista_ordinal[i]} jugador es: {jugadores[i].nombre}  \n'
+        print(cadena)
+               
+    def mostrar_turno(jugadores,i):#8
+        time.sleep(1)
+        print(f"\nTurno de {colored(jugadores[i].nombre.upper(),attrs=['underline'])}:")
         time.sleep(1)
       
-    def mostrar_ganador(jugadores,i):
+    def mostrar_ganador(jugadores,i):#9
         print(f"\n¡{jugadores[i].nombre.upper()} ha ganado!") 
     
-    def guardar_resultado(jugador,tablero,jugadores,tableros):
+    def guardar_resultado(jugador,tablero,jugadores,tableros):#10
         resultados = [f"El jugador {jugador.nombre} obtuvo {tablero.queso_instancia.cantidad_quesos()}" for jugador, tablero in zip(jugadores, tableros)]
         return resultados
     
+    def pregunta_final():#11
+        final=input("¿Deseas seguir jugando? (si/no): ")
+        mantener_jugadores=input('¿Deseas mantener los jugadores actuales? (si/no)')
+        return final, mantener_jugadores
+   
     #Métodos generales
     def errores_excepciones_general():
-        texto=colored('Ha sucedido un error!!','red')
+        texto=colored('Ha sucedido un error!!','red',attrs=['bold',])
         print(texto)
         
+    def errores_excepciones_value():
+        print('Introduce un valor correcto.')
         
         
     #Métodos Clase Tablero
     def preguntar_movimiento(casilla,dado):
-        movimiento=input(f"Estás en la casilla {casilla} Quieres {colored('AVANZAR (A)','green')} o {colored('RETROCEDER (R)','red')}: {dado} casillas ").lower()
+        print('Tirando dado', end='', flush=True)
+        time.sleep(0.3)
+        print('.', end='', flush=True)
+        time.sleep(0.3)
+        print('.', end='', flush=True)
+        time.sleep(0.4)
+        print('.', end='\n', flush=True)
+        time.sleep(2)
+        
+        print(f'Haz sacado un {colored(dado,'yellow',attrs=['bold'])} en el dado', flush=True)
+        if casilla%7==0 and casilla!=0:
+            movimiento=input(f"Estás en la casilla {colored(casilla,'yellow',attrs=['bold'])} Quieres {colored('AVANZAR (A)','green')} o {colored('RETROCEDER (R)','red')}: ").lower()
+        else:
+            movimiento=input(f"Estás en la casilla {casilla} Quieres {colored('AVANZAR (A)','green')} o {colored('RETROCEDER (R)','red')}: ").lower()
+
         return movimiento
     
     def imprimir_opcion_correcta():
@@ -132,7 +173,8 @@ class Vista:
         print(f'Cantidad de quesos obtenidos: {quesos}')
         
     def imprimir_pregunta(categoria,pregunta):
-        print(f'\nLa categoría es {colored(categoria, attrs=["bold"])}')
+        
+        print(f'\nLa categoría es {colored(categoria.upper(), attrs=["bold"])}')
         print(f"\n{pregunta['enunciado']}")
         for opcion in pregunta['opciones']:
             print(opcion)
